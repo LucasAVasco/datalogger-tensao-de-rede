d5fa153985174d75c7881632353593ac91fc7c09
LucasAVasco <none>
2021-03-20 15:42:50 -0300

    Simulação do arduino
    Adicionado simulação do arduino: 'simulacao.ino'. A diferença dele para o 'debug.ino' é
    que o 'debug.ico' buscava apenas testar e portotipar o código.
    Inclusive, tinha um delay para facilitar isso. O 'simulacao.ino' busca,
    realmente, simular o código normal que irá no arduino.

---
+++ arduino/simulacao.ino
@@ -0,0 +1,44 @@
+#include <math.h>
+
+
+/// Macros
+#define ANALOG_PIN A0
+
+
+
+/// Variáveis
+int vpk;
+float value = 0;
+
+
+
+/// Inicialização
+void setup()
+{
+	// Inicia a comunicação serial
+	Serial.begin(9600);
+
+	// Espera até o código python estiver carregado
+	while (Serial.available() == 0)
+	{
+		delay(100);
+	}
+}
+
+
+
+/// Loop principal
+void loop()
+{
+	// Gera o atraso da leitura de tensão
+	vpk = analogRead(ANALOG_PIN);
+
+	// Para prototipação
+	value = float (micros()) * 120 * M_PI / 1000000;
+	vpk = int ((sin(value) + 1.0) * 511);
+
+
+	// Envia o inteiro em dois bytes
+	if (Serial.write(vpk >> 8))
+		Serial.write(vpk);
+}
