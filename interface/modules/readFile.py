8f2e6773161de14ed27b4504d158156c52a97d08
LucasAVasco <none>
2021-05-05 16:43:35 -0300

    Melhorado comentários do módulo 'readFile.py'
    Arquivo dentro do diretporio 'interface/modules/'.
    
    Melhorado comentários do código que remove os espaços no início e
    final da linha lida.

--- interface/modules/readFile.py
+++ interface/modules/readFile.py
@@ -22,12 +22,12 @@ def getNextLine(input_file):
 
     # Fomatatando a linha
     line = re.sub("\r", '', line)     # Compatibilidade com windows
-    line = re.sub("\n$", '', line)     # New Line
+    line = re.sub("\n$", '', line)    # New Line
     line = re.sub("^\t*", '', line)   # Tabulação
     line = re.sub("#.*$", '', line)   # Comentários de python
     line = re.sub("//.*$", '', line)  # Comentários de c++
-    line = re.sub("^ *", '', line)    # Espaços no início
-    line = re.sub(" *$", '', line)    # Espaços no final
+    line = re.sub("^ *", '', line)    # Espaços no início da linha
+    line = re.sub(" *$", '', line)    # Espaços no final da linha
 
     return line
 
