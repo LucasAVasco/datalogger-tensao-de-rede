bd1b895edf3de707da170d2ff8ff5cd8d11463f3
LucasAVasco <none>
2021-05-05 18:21:52 -0300

    Bug do comentários do arquivo de configuração
    Corrigido um bug que finalizava a leitura do arquivo de configuração da
    interface web ("config.txt" no diretório "interface/") no caso de haver
    um comentário de linha inteira.

--- interface/modules/readFile.py
+++ interface/modules/readFile.py
@@ -14,22 +14,27 @@ import re
 
 def getNextLine(input_file):
     """Retorna a proxima linha já formatada."""
-    line = input_file.readline()
-
-    # Espera até receber uma linha diferente de '\n'
-    while line == '\n':
+    while True:
         line = input_file.readline()
 
-    # Fomatatando a linha
-    line = re.sub("\r", '', line)     # Compatibilidade com windows
-    line = re.sub("\n$", '', line)    # New Line
-    line = re.sub("^\t*", '', line)   # Tabulação
-    line = re.sub("#.*$", '', line)   # Comentários de python
-    line = re.sub("//.*$", '', line)  # Comentários de c++
-    line = re.sub("^ *", '', line)    # Espaços no início da linha
-    line = re.sub(" *$", '', line)    # Espaços no final da linha
+        # Se chegou no final do arquivo
+        if line == '':
+            return ''
 
-    return line
+        # Fomatatando a linha
+        line = re.sub("\r", '', line)     # Compatibilidade com windows
+        line = re.sub("\n$", '', line)    # New Line
+        line = re.sub("^\t*", '', line)   # Tabulação
+        line = re.sub("#.*$", '', line)   # Comentários de python
+        line = re.sub("//.*$", '', line)  # Comentários de c++
+        line = re.sub("^ *", '', line)    # Espaços no início da linha
+        line = re.sub(" *$", '', line)    # Espaços no final da linha
+
+        # Se a linha não possui nenhum instrução
+        if line == '':
+            continue  # Le a próxima linha
+
+        return line
 
 
 def format_value(value):
